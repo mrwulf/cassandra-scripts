ee3cb3babf28e4f7d3d41c22d7b6205c46027625
Brandon Wulf <mrwulf@gmail.com>
2016-04-06 16:48:55 -0700

    Adding some helpful options for untilavailable command

--- untilavailable.sh
+++ untilavailable.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-until STAT=`nodetool statusbinary 2>&1` && [ "$STAT"  == "running" ]; do 
-  echo "Not Available..."; 
+# Setup
+OPTIND=1;
+HOST="";
+
+function help() {
+  echo "$0 [-h|--host HOST] [-v|--verbose] [-1|--one] [--help]";
+}
+
+while [[ $# > 0 ]]; do
+  case "$1" in
+    -h|--host)
+      HOST="-h $2";
+      shift 2;
+      ;;
+    -v|--verbose)
+      VERBOSE=true;
+      shift 1;
+      ;;
+    -1|--one)
+      ONEONLY=true;
+      shift 1;
+      ;;
+    --help)
+      help;
+      exit 0;
+      ;;
+  esac
+done
+
+until STAT=`nodetool $HOST statusbinary 2>&1` && [ "$STAT"  == "running" ]; do 
+  [[ $VERBOSE ]] && echo "Not Available..."; 
+  [[ $ONEONLY ]] && exit 1;
   sleep 2; 
 done
 
-echo "Available!";
+[[ $VERBOSE ]] && echo "Available!";
+exit 0;
